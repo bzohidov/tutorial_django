b737e506f68c1d026f1c5112f9b508865d269828
Bahtiyor Zohidov <bahtiyor@cbien.com>
2017-02-06 12:35:42 +0100

    makemigrations of Album and Song models

--- music/models.py
+++ music/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 
 
-class Person(models.Model):
-	name = models.CharField(max_length=200)
-
-
 class Album(models.Model):
 	artist = models.CharField(max_length=250)
 	album_title = models.CharField(max_length=500)
@@ -13,7 +9,7 @@ class Album(models.Model):
 	album_logo = models.CharField(max_length=1000)
 	
 	author = models.CharField(max_length=200)
-	album_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
+	#album_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
 
 	def __str__(self):
 		return self.album_title + ' - ' + self.artist 
@@ -24,8 +20,12 @@ class Song(models.Model):
 	file_type = models.CharField(max_length=10)
 	song_title = models.CharField(max_length=250)
 
-	song_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
+	#song_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
 	singer = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.song_title
+
+
+class Person(models.Model):
+	name = models.CharField(max_length=200)
