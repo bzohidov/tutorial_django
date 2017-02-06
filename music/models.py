f2c4d370217ec46c1b8364eb3c0387461d103db9
Bahtiyor Zohidov <bahtiyor@cbien.com>
2017-02-06 12:21:12 +0100

    Create 'Person' model and connect with Album, Song models

--- music/models.py
+++ music/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 
 
+class Person(models.Model):
+	name = models.CharField(max_length=200)
+
+
 class Album(models.Model):
 	artist = models.CharField(max_length=250)
 	album_title = models.CharField(max_length=500)
 	genre = models.CharField(max_length=100)
 	album_logo = models.CharField(max_length=1000)
+	
+	author = models.CharField(max_length=200)
+	album_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
 
 	def __str__(self):
 		return self.album_title + ' - ' + self.artist 
@@ -17,18 +24,8 @@ class Song(models.Model):
 	file_type = models.CharField(max_length=10)
 	song_title = models.CharField(max_length=250)
 
-	def __str__(self):
-		return self.song_title
-
-
-
-class Album1(models.Model):
-	author = models.CharField(max_length=200)
-
-
-class Song1(models.Model):
+	song_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
 	singer = models.CharField(max_length=200)
 
-
-class Person(models.Model):
-	name = models.CharField(max_length=200)
+	def __str__(self):
+		return self.song_title
