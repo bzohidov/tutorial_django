2325769fe393fe137f722c3932123cf6072c7b79
Bahtiyor Zohidov <bahtiyor@cbien.com>
2017-02-07 18:56:18 +0100

    adding new migrations

--- music/models.py
+++ music/models.py
@@ -2,30 +2,32 @@ from django.db import models
 
 
 
+class Person(models.Model):
+	name = models.CharField(max_length=200)
+	#birthday = models.DateTimeField(max_length=200)
+
+
 class Album(models.Model):
+	#album_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
 	artist = models.CharField(max_length=250)
 	album_title = models.CharField(max_length=500)
 	genre = models.CharField(max_length=100)
 	album_logo = models.CharField(max_length=1000)
-	
 	author = models.CharField(max_length=200)
-	#album_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
+
+	person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
 
 	def __str__(self):
 		return self.album_title + ' - ' + self.artist 
 
 
 class Song(models.Model):
-	album = models.ForeignKey(Album, on_delete=models.SET_NULL, null=True)
 	file_type = models.CharField(max_length=10)
 	song_title = models.CharField(max_length=250)
-
-	#song_person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
 	singer = models.CharField(max_length=200)
+
+	album = models.ManyToManyField('Album')
+	person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
 
 	def __str__(self):
 		return self.song_title
-
-
-class Person(models.Model):
-	name = models.CharField(max_length=200)
